2f4db9e51bf0a1f68f309870f49a9c40db5124a4
yassmim07 <131712345+yassmim07@users.noreply.github.com>
2023-04-24 17:04:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Input */
+/* prompt é basicamente a mesma coisa que o input */
+num1 = prompt('Digite o primerio valor:')
+num2 = prompt('Digite o segundo valor:')
+
+/* Processamento */
+/*parseint serve para converter textos para números  */
+soma = parseInt(num1) + parseInt(num2)
+
+/* Output */
+window.alert('A soma é ' + soma)
